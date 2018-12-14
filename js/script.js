c4e89f2f1a24152727e513614bbf2c28adf45926
nyirahabimana dative <nyiradative052@gmail.com>
2018-12-14 15:31:53 +0200

    create files

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+function main () {
+    var side1=parseInt(document.getElementById('side1').value);
+    var side2=parseInt(document.getElementById('side2').value);
+    var side3=parseInt(document.getElementById('side3').value);
+
+if (side1 === side2>0 && side1 === side3>0 && side2===side3>0) {
+    console.log(alert('your triangle is equilateral'))
+}else if (side1 === side2 || side1 !== side3 || side2 !== side3) {
+    console.log(alert ('your triangle is isosceles')) 
+}else if (side1 !==side2 && side1 !== side3 && side2 !==side3) {
+    console.log(alert ('your triangle is scalene'))
+}else{
+    console.log(alert("this is not a triangle"))
+}
+}
